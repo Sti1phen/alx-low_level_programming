66a731ac989201533e759cea2dbb14b0eb579fae
Sti1phen <107007210+Sti1phen@users.noreply.github.com>
2022-07-01 00:43:28 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-mmm
+#include <stdio.h>
+/**
+* main - Write '"Programming is like building a multilingual puzzle'
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
