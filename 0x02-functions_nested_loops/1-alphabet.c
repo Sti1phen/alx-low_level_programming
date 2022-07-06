d15859da85329208a45fd2eb200ab16be5f93066
Sti1phen <107007210+Sti1phen@users.noreply.github.com>
2022-07-06 21:28:46 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,16 @@
-dd
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ * Return: 0 Success
+ */
+void print_alphabet(void)
+{
+	char j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
